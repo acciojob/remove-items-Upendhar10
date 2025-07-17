826a061d0f56744b610498f6d66ee8b4c9a60678
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-07-17 16:39:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let Btn = document.querySelector('input[type="button"]');
+
+Btn.addEventListener("click", RemoveOption);
+
+function RemoveOption(e){
+	let dropdown = document.getElementById('colorSelect');
+	let list = dropdown.options;
+	let OptionSelected = list.selectedIndex;
+	if (OptionSelected !== -1) {
+    dropdown.remove(OptionSelected);
+  }
+}
